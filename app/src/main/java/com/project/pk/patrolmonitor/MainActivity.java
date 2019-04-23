5e819c11fa44e39f28188ac5d6eca5f48442786d
PoojaK97 <thisispooja97@gmail.com>
2019-04-23 09:38:08 +0530

    added a button

--- app/src/main/java/com/project/pk/patrolmonitor/MainActivity.java
+++ app/src/main/java/com/project/pk/patrolmonitor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.project.pk.patrolmonitor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -29,6 +31,15 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Logged In", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button bmap = (Button)findViewById(R.id.button);
+        bmap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), MapsActivity.class);
+                startActivity(myIntent);
             }
         });
 
