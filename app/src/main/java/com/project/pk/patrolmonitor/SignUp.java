afc5a0540ca9a50980493d3274ed3f2579c1494c
PoojaK97 <thisispooja97@gmail.com>
2019-04-25 01:32:28 +0530

    ..

--- app/src/main/java/com/project/pk/patrolmonitor/SignUp.java
+++ app/src/main/java/com/project/pk/patrolmonitor/SignUp.java
@@ -15,6 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Context;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
