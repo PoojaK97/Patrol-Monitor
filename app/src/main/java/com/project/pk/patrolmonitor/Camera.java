f76029444506cce643960c7b6f1b91ff3583ea8e
PoojaK97 <thisispooja97@gmail.com>
2019-04-25 22:07:22 +0530

    Uploading to Cloud

--- app/src/main/java/com/project/pk/patrolmonitor/Camera.java
+++ app/src/main/java/com/project/pk/patrolmonitor/Camera.java
@@ -2,7 +2,9 @@ package com.project.pk.patrolmonitor;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +13,26 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import android.net.Uri;
 
+import java.io.ByteArrayOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 public class Camera extends AppCompatActivity {
     private Button btnCapture;
     private Button btnCapture1;
     private ImageView imgCapture;
+    private String filename;
+    //private Uri imageFileUri;
+    private StorageReference mStorageRef;
     private static final int Image_Capture_Code = 1;
     private static final int VIDEO_CAPTURE = 101;
 
@@ -25,11 +43,14 @@ public class Camera extends AppCompatActivity {
         btnCapture =(Button)findViewById(R.id.btnTakePicture);
         btnCapture1 =(Button)findViewById(R.id.button1);
         imgCapture = (ImageView) findViewById(R.id.capturedImage);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         btnCapture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(cInt,Image_Capture_Code);
+
             }
         });
         btnCapture1.setOnClickListener(new View.OnClickListener() {
@@ -44,8 +65,7 @@ public class Camera extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == Image_Capture_Code) {
             if (resultCode == RESULT_OK) {
-                Bitmap bp = (Bitmap) data.getExtras().get("data");
-                imgCapture.setImageBitmap(bp);
+
 
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
@@ -56,6 +76,30 @@ public class Camera extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 Toast.makeText(this, "Video saved to:\n" +
                         videoUri, Toast.LENGTH_LONG).show();
+
+                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                Date date = new Date();
+                filename ="VID_"+ dateFormat.format(date).replaceAll("\\s+","").replaceAll("/","_");
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = user.getUid();
+                StorageReference riversRef = mStorageRef.child("Beats/"+uid+"/"+filename);
+
+                riversRef.putFile(videoUri)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                // Get a URL to the uploaded content
+                                //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                                System.out.println("sucess upload");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle unsuccessful uploads
+                                // ...
+                            }
+                        });
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Video recording cancelled.",
                         Toast.LENGTH_LONG).show();
